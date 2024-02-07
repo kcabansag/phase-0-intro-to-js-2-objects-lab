9c614c7c9fbcb28c3f56b96c4794ea73573bd76c
Kris Janssen Cabansag <kjs.cabansag@gmail.com>
2024-02-07 17:29:30 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Kris",
+    streetAddress: "Thomas St",
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const copyEmployee = {...employee};
+    delete copyEmployee[key];
+return copyEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+    delete employee[key];
+    return employee;
+}
+
